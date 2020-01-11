5465b2a2136b9ddde10ce7037e0ef6296315636d
aakbathl <aakash.bathla@sapient.com>
2020-01-12 01:21:56 +0530

    useState

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
@@ -9,17 +9,20 @@ const IngredientForm = React.memo(props => {
     // ...
   };
 
+  const [enteredName,setEnteredName] = useState('');
+  const [enteredAmount,setEnteredAmout] = useState('');
+
   return (
     <section className="ingredient-form">
       <Card>
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title" value={enteredName} onChange = {event => {setEnteredName(event.target.value)}}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount" value={enteredAmount} onChange = {event => {setEnteredAmout(event.target.value)}}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
