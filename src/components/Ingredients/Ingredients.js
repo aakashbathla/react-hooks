d14f8246ed1d0d1063dcd7ac0f5d075c6b19dab2
aakbathl <aakash.bathla@sapient.com>
2020-01-12 09:09:01 +0530

    get ingredients | useEffects

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,11 +1,29 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
-  const [userIngredients,setUserIngredients] = useState([])
+  const [userIngredients,setUserIngredients] = useState([]);
+  useEffect(()=> {
+    fetch('https://react-hooks-cb078.firebaseio.com/ingredients.json',{
+      method: 'GET',
+      headers: {'Content-Type': 'applicaiton/json'}
+    }).then(response => {
+      return response.json()
+    }).then(responseData => {
+      const loadedIngredient = [];
+      for(const key in responseData){
+        loadedIngredient.push({
+          id: key,
+          name: responseData[key].name,
+          amount: responseData[key].amount
+        })
+      }
+      setUserIngredients(loadedIngredient);
+    })
+  },[])
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-cb078.firebaseio.com/ingredients.json',{
         method: 'POST',
