47b91f868cdd73395ec907ebb8537bcd2b885747
aakbathl <aakash.bathla@sapient.com>
2020-01-12 12:15:13 +0530

    batches state updated and loading errors

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,27 +3,12 @@ import React,{useState,useEffect,useCallback} from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 function Ingredients() {
   const [userIngredients,setUserIngredients] = useState([]);
-  // useEffect(()=> {
-  //   fetch('https://react-hooks-cb078.firebaseio.com/ingredients.json',{
-  //     method: 'GET',
-  //     headers: {'Content-Type': 'applicaiton/json'}
-  //   }).then(response => {
-  //     return response.json()
-  //   }).then(responseData => {
-  //     const loadedIngredient = [];
-  //     for(const key in responseData){
-  //       loadedIngredient.push({
-  //         id: key,
-  //         name: responseData[key].name,
-  //         amount: responseData[key].amount
-  //       })
-  //     }
-  //     setUserIngredients(loadedIngredient);
-  //   })
-  // },[]);
+  const [isLoading,setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(()=>{
     console.log('RENDERING INGREDIENTS',userIngredients);
@@ -34,11 +19,13 @@ function Ingredients() {
   },[])
 
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://react-hooks-cb078.firebaseio.com/ingredients.json',{
         method: 'POST',
         headers: {'Content-Type':'application/json'},
         body: JSON.stringify(ingredient)
     }).then(response => {
+      setIsLoading(false);
       return response.json();
     }).then(responseData => {
       setUserIngredients(prevIngredients => [
@@ -51,20 +38,27 @@ function Ingredients() {
   };
 
   const removeIngredientHandler = ingredientId => {
+    setIsLoading(true)
     fetch(`https://react-hooks-cb078.firebaseio.com/ingredients/${ingredientId}.json`,{
         method: 'DELETE',
         headers: {'Content-Type':'application/json'}
     }).then((response)=>{
-      debugger
+      setIsLoading(false)
       setUserIngredients(prevIngredients=>
         prevIngredients.filter(ingredient=>ingredient.id !== ingredientId)
       )
+    }).catch((error)=>{
+      setError('Something Went Wrong!');
     })
   }
-
+  const clearError = () => {
+    setError(null);
+    setIsLoading(false);
+  }
   return (
     <div className="App">
-      <IngredientForm onAddIngredient = {addIngredientHandler}/>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm onAddIngredient = {addIngredientHandler} loading = {isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientHandler} />
